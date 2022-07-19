08119055df652956df43239bfaee6a14538462e8
Margi Bhatt <bhattmargi27@gmail.com>
2022-07-19 20:55:52 +0100

    MyFirstJavaProgram

---
+++ MyFirstProgram.java
@@ -0,0 +1,9 @@
+package Javaconcept;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("My First Program");
+        System.out.println("Margi");
+
+    }
+}
