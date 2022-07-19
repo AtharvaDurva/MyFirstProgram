d09a22016a7d12e8809355b01943ee7b04ddbd57
Margi Bhatt <bhattmargi27@gmail.com>
2022-07-19 21:08:08 +0100

    updated Myfirstprogram

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -4,6 +4,7 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("My First Program");
         System.out.println("Margi");
+        System.out.println("I am ok but not stable");
 
     }
 }
